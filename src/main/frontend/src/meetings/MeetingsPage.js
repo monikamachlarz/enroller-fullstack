8ec7e7b65d808396be68342023a5dc72b0c80951
machlarz <mimac1982@gmail.com>
2025-05-11 16:01:53 +0200

    deleteMeetings

--- src/main/frontend/src/meetings/MeetingsPage.js
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -30,10 +30,22 @@ export default function MeetingsPage({username}) {
         }
 
     }
-    function handleDeleteMeeting(meeting) {
-        const nextMeetings = meetings.filter(m => m !== meeting);
-        setMeetings(nextMeetings);
+    async function handleDeleteMeeting(meeting) {
+        const response = await fetch(`/api/meetings/${meeting.id}`, {
+            method: 'DELETE',
+        });
+        if (response.ok) {
+            const nextMeetings = [...meetings, meeting];
+            //const nextMeetings = meetings.filter(m => m !== meeting);
+            setMeetings(nextMeetings);
+        }
+
     }
+
+    // function handleDeleteMeeting(meeting) {
+    //     const nextMeetings = meetings.filter(m => m !== meeting);
+    //     setMeetings(nextMeetings);
+    // }
 
     return (
         <div>
